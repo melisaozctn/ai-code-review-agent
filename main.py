4f60a318778792e88f34d7dd37cdba9e6ece2f26
Melisa Ozcetin <melisaozctn@gmail.com>
2026-05-13 16:41:31 +0300

    Connected review agent to main application

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("AI Code Review Agent")
+from src.agent.review_agent import ReviewAgent
+
+
+sample_code = """
+def add(a, b):
+    return a + b
+"""
+
+
+agent = ReviewAgent()
+
+results = agent.review_code(sample_code)
+
+print(results)
